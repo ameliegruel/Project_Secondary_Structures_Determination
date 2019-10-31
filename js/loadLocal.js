3897345b4b263069bce14b29dc330b2124a1af11
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2019-10-31 14:54:44 +0100

    code base

---
+++ js/loadLocal.js
@@ -0,0 +1,21 @@
+/*
+* Load / Parse Local PDB file
+* 2019/10/18
+* Eliot Ragueneau
+* */
+
+const getContents = (event) => {
+  let f = event.target.files[0];
+
+  let reader = new FileReader();
+  reader.onload = ev => {
+    let text = reader.result;
+    let struct = parsePDB(text);
+    console.log(struct);
+    let second_struct = getSecondaryStructure(struct);
+  };
+  reader.readAsText(f);
+
+};
+
+$('#pdb').on("change", getContents);
