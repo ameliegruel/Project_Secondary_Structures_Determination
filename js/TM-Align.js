b4b84b25c0adf2607ea8a87053311bec6451950b
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2019-10-31 15:36:06 +0100

    functionnal constant giving

--- js/TM-Align.js
+++ js/TM-Align.js
@@ -1,31 +1,43 @@
 const getSecondaryStructure = (structure) => {
-  let carbons = getAlphaCarbons(structure);
-  console.log(carbons);
-  let chains = splitAtomsIntoChains(carbons);
-  console.log(chains);
-  let chainA = chains["A"];
-  let distMatrix = chainA.reduce();
+    let carbons = getAlphaCarbons(structure);
+    console.log(carbons);
+    let chains = splitAtomsIntoChains(carbons);
+    console.log(chains);
+    let chainA = chains["A"];
+    let distMatrix = chainA.reduce();
 
-  return [];
+    return [];
 };
 
 const getAlphaCarbons = (structure) => {
-  return structure.atoms.filter(atom => atom.name === "CA");
+    return structure.atoms.filter(atom => atom.name === "CA");
 };
 
 const splitAtomsIntoChains = (atomList) => {
-  return atomList.reduce((chains, atom) => {
-    if (chains[atom.chainID] === undefined) {
-      chains[atom.chainID] = [];
-    }
-    chains[(atom.chainID)].push(atom);
-    return chains;
-  }, {})
+    return atomList.reduce((chains, atom) => {
+        if (chains[atom.chainID] === undefined) {
+            chains[atom.chainID] = [];
+        }
+        chains[(atom.chainID)].push(atom);
+        return chains;
+    }, {})
 };
 
 const distanceBetweenAtoms = (atomA, atomB) => {
-  return Math.sqrt(
-    Math.pow(atomA.x - atomB.x, 2) +
-    Math.pow(atomA.y - atomB.y, 2) +
-    Math.pow(atomA.z - atomB.z, 2))
+    return Math.sqrt(
+        Math.pow(atomA.x - atomB.x, 2) +
+        Math.pow(atomA.y - atomB.y, 2) +
+        Math.pow(atomA.z - atomB.z, 2))
+};
+
+const lambda = (ssType) => {
+    return ssType === "alpha" ? (distanceK) => {
+        return distanceK === 2 ? 5.45 : distanceK === 3 ? 5.18 : distanceK === 4 ? 6.37 : new Error("Undefined distance k");
+    } : ssType === "beta" ? (distanceK) => {
+        return distanceK === 2 ? 6.1 : distanceK === 3 ? 10.4 : distanceK === 4 ? 13 : new Error("Undefined distance k");
+    } : new Error("Undefined ssType");
+};
+
+const delta = (ssType) => {
+    return ssType === "alpha" ? 2.1 : ssType === "beta" ? 1.42 : new Error("Undefined ssType");
 };
