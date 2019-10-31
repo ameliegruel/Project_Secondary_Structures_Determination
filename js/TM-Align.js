5225f74061ceb99e4927f61cd09a59bf9bf1e159
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2019-10-31 16:37:18 +0100

    constant object for delta and lambdas

--- js/TM-Align.js
+++ js/TM-Align.js
@@ -7,41 +7,23 @@ const getSecondaryStructure = (structure) => {
     let chainA = chains["A"];
     let distMatrix = chainA.reduce();
 
-    return [];
+    return [].reduce();
 };
 
 // récupère les atomes Ca
-const getAlphaCarbons = (structure) => {
-    return structure.atoms.filter(atom => atom.name === "CA");
-};
+const getAlphaCarbons = (structure) => structure.atoms.filter(atom => atom.name === "CA");
 
 // sépare les pdb en chaines 
-const splitAtomsIntoChains = (atomList) => {
-    return atomList.reduce((chains, atom) => {
-        if (chains[atom.chainID] === undefined) {
-            chains[atom.chainID] = [];
-        }
-        chains[(atom.chainID)].push(atom);
-        return chains;
-    }, {})
-};
+const splitAtomsIntoChains = (atomList) => atomList.reduce((chains, atom) => {
+    chains[atom.chainID] = chains[atom.chainID] ? [...chains[atom.chainID], atom] : [atom];
+    chains[atom.chainID].push(atom);
+    return chains;
+}, {});
+
 
 // obtenir la distance euclidienne entre les atomes en 3D
-const distanceBetweenAtoms = (atomA, atomB) => {
-    return Math.sqrt(
-        Math.pow(atomA.x - atomB.x, 2) +
-        Math.pow(atomA.y - atomB.y, 2) +
-        Math.pow(atomA.z - atomB.z, 2))
-};
+const distanceBetweenAtoms = (atomA, atomB) => Math.sqrt(Math.pow(atomA.x - atomB.x, 2) + Math.pow(atomA.y - atomB.y, 2) + Math.pow(atomA.z - atomB.z, 2));
 
-const lambda = (ssType) => {
-    return ssType === "alpha" ? (distanceK) => {
-        return distanceK === 2 ? 5.45 : distanceK === 3 ? 5.18 : distanceK === 4 ? 6.37 : new Error("Undefined distance k");
-    } : ssType === "beta" ? (distanceK) => {
-        return distanceK === 2 ? 6.1 : distanceK === 3 ? 10.4 : distanceK === 4 ? 13 : new Error("Undefined distance k");
-    } : new Error("Undefined ssType");
-};
+const lambda = {alpha: {2: 5.45, 3: 5.18, 4: 6.37}, beta: {2: 6.1, 3: 10.4, 4: 13}};
 
-const delta = (ssType) => {
-    return ssType === "alpha" ? 2.1 : ssType === "beta" ? 1.42 : new Error("Undefined ssType");
-};
+const delta = {alpha: 2.1, beta: 1.42};
